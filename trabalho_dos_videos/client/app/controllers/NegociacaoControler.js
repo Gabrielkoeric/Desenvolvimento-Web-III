f2b6c921c8fdccb86fbf8fdc6918569c11e69f79
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2022-10-20 20:36:06 -0300

    até video 14

---
+++ trabalho_dos_videos/client/app/controllers/NegociacaoControler.js
@@ -0,0 +1,42 @@
+class NegociacaoController{
+
+    constructor(){
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+        this.negiciacoes = new Negociacoes();
+        this._negociacoesView = NegociacoesView('#negociacoes');
+        this._negociacoesView = update(this._negociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView('#mensagemView')
+        this._mensagemView.update(this._mensagem);
+
+    }
+
+    adiciona(event){
+        event.preventDefault();
+
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'negociação adicionada com sucesso';
+        this._negociacoesView.update(this._negiciacoes);
+        this._mensagemView.update(this._mensagem);
+        this._limparFormulario();
+    }
+
+    _limparFormulario(){
+        this._inputData.value = '2020-01-01';
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 2.0
+        this._inputData.focus();
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        )
+    }
+}
---
+++ trabalho_dos_videos/client/app/domain/negociacao/Negociacoes.js
@@ -0,0 +1,23 @@
+class Negociacoes{
+
+    constructor(){
+        this._negociacoes = []
+    }
+
+    adiciona(negociacao){
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray() {
+        return [].concat(this._negociacoes);
+    }
+
+    get volumeTotal(){
+        //let total = 0;
+        //for (let i = 0; i < this._negociacoes.length; i++){
+          //  total+= this._negociacoes[i].volume;
+        //}
+        //return total;
+        return this._negociacoes.reduce((total, negiciacao) => total + negiciacao.volume, 0);
+    }
+}
